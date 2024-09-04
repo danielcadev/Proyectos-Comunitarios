621a941f7be120173021625eafd5ae2ada1f102b
DarkyDev5 <daniel.ca.pe207@gmail.com>
2024-09-04 18:22:58 -0500

    chore: Update start script to log Node version and environment

--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@
 
 module.exports = {
   register({ strapi }) {
+    console.log('Registering middlewares...');
     strapi.server.use(async (ctx, next) => {
       if (ctx.path === '/healthz') {
+        console.log('Health check requested');
         ctx.body = 'OK';
         ctx.status = 200;
       } else {
         await next();
       }
     });
+    console.log('Middlewares registered');
   },
 
-  bootstrap({ strapi }) {
-    // Cualquier otra lógica de bootstrap que puedas tener
+  async bootstrap({ strapi }) {
+    console.log('Starting bootstrap process...');
+    // Tu lógica de bootstrap existente, si la hay
+    console.log('Bootstrap process completed');
   },
 };
