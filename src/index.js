ab5cf9ff59ea7d18093b3266e7e20d937cd969d5
DarkyDev5 <daniel.ca.pe207@gmail.com>
2024-09-04 18:18:43 -0500

    chore: Update healthcheck timeout to 300

--- src/index.js
+++ src/index.js
@@ -1,22 +1,7 @@
 'use strict';
 
 module.exports = {
-  /**
-   * An asynchronous register function that runs before
-   * your application is initialized.
-   *
-   * This gives you an opportunity to extend code.
-   */
-  register(/*{ strapi }*/) {},
-
-  /**
-   * An asynchronous bootstrap function that runs before
-   * your application gets started.
-   *
-   * This gives you an opportunity to set up your data model,
-   * run jobs, or perform some special logic.
-   */
-  bootstrap({ strapi }) {
+  register({ strapi }) {
     strapi.server.use(async (ctx, next) => {
       if (ctx.path === '/healthz') {
         ctx.body = 'OK';
@@ -25,5 +10,9 @@ module.exports = {
         await next();
       }
     });
+  },
+
+  bootstrap({ strapi }) {
+    // Cualquier otra lógica de bootstrap que puedas tener
   },
 };
